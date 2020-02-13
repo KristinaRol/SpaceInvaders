e1da18b51408d3f2b10e5070e3d5edf9b44327d3
KristinaRol <rolsing.kristina@gmail.com>
2020-02-13 14:04:55 +0100

    added bomb class in enemy

--- SpaceInvaders/src/Model/Enemy.java
+++ SpaceInvaders/src/Model/Enemy.java
@@ -1,40 +1,78 @@
 package Model;
 
+import javax.swing.ImageIcon;
+
 public class Enemy {
 
 	private double x;
 	private double y;
 	private int hitpoints = 10;
-	//private Bomb bomb;
-	
+	private Bomb bomb;
+
 	public Enemy(double x, double y) {
-		initEnemy(x,y);
+		initEnemy(x, y);
 	}
-	
-	//wird noch fertig gestellt mit bomb und so
+
+	// wird noch fertig gestellt mit bomb und so
 	private void initEnemy(double x, double y) {
 		this.x = x;
 		this.y = y;
+
+		bomb = new Bomb(x, y);
 	}
-	
+
+	public void move(double y) {
+		this.y += y;
+	}
+
+
+	public Bomb getBomb() {
+		return bomb;
+	}
+
 	protected double getX() {
 		return x;
 	}
+
 	protected void setX(double x) {
 		this.x = x;
 	}
+
 	protected double getY() {
 		return y;
 	}
+
 	protected void setY(double y) {
 		this.y = y;
 	}
+
 	protected int getHitpoints() {
 		return hitpoints;
 	}
+
 	protected void setHitpoints(int hitpoints) {
 		this.hitpoints = hitpoints;
 	}
-	
-	
+
+	public class Bomb {
+		private boolean destroyed = false;
+		double x, y;
+
+		public Bomb(double x, double y) {
+			setDestroyed(true);
+			this.x = x;
+			this.y = y;
+		}
+		
+		public void setDestroyed(boolean destroyed) {
+
+            this.destroyed = destroyed;
+        }
+
+        public boolean isDestroyed() {
+
+            return destroyed;
+        }
+	}
+
 }
